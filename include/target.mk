f3069bf9a8854220985602d017c3a0bbac1c02fc
benjaminbnuz <1411030061@mail.bnuz.edu.cn>
2019-12-09 22:06:12 +0800

    Update target.mk

--- include/target.mk
+++ include/target.mk
@@ -15,6 +15,7 @@ DEVICE_TYPE?=router
 # Default packages - the really basic set
 DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd block-mount coremark \
 kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw wget ca-certificates bash \
+openwrt-adguardhome-master luci-app-adguardhome-master \
 default-settings luci-app-adbyby-plus coreutils \
 luci-app-filetransfer luci-app-ssr-plus luci-app-opencash coreutils-nohup \
 luci-app-sfe luci-app-flowoffload luci-app-nlbwmon luci-app-zerotier
